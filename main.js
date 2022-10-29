fa603704d33f69969540a30370282d3744f9eaa3
Luis Hernandez <lshrnndz6@gmail.com>
2022-10-29 12:36:14 -0700

    commit tree d463ac33f58e64838a01a56fdb30c4dc1e3810df

--- main.js
+++ main.js
@@ -3,9 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let likes = document.querySelectorAll('.like-glyph')
 
+for (heart of likes) {
+  heart.addEventListener('click', manageLike)
+}
 
+function manageLike(event) {
+ const targetHeart = event.target;
 
+  mimicServerCall()
+    .then(function (response) {
+      if(targetHeart.innerText === EMPTY_HEART) {
+        targetHeart.classList.add('activated-heart')
+        targetHeart.textContent = FULL_HEART;
+      } else {
+        targetHeart.classList.remove('activated-heart')
+        targetHeart.textContent = EMPTY_HEART;
+      }
+    })
+    .catch(function (error) {
+      const modal = document.querySelector('#modal')
+      console.log(modal)
+      modal.classList.remove('hidden')
+      modal.innerText = error
+      setTimeout(function () {
+        modal.classList.add('hidden')
+      }, 3000)
+    });
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -23,3 +49,23 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+// Action Plan:
+  
+// #. do 1a -1b in console  
+ 
+// 1a. select any heart
+// 1b. Get rid of the full heart by getting rid of .activated-heart
+
+// /// once you do steps above ///
+
+// 2b. create an if statement that checks to see if there is a full heart on the page
+// and so do...
+
+// Change the heart back to an EMPTY_HEART  
+// Remove the .activated-heart class
+
+// But if not do...
+
+// Change the heart to a FULL_HEART  
+// Add the .activated-heart class to make the heart appear red
